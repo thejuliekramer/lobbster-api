26bf7de87a7a84f6e4a16edbfb1d0f073c8e2333
Julie <jkramer@mdlive.com>
2017-11-05 07:24:58 -0500

    Initial commit setep postgres db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lobbster::Application.config.secret_token = 'c6fff17100d3bff1d22cf646bc93bb75b11eeb1d3a5fb66bdd60340382fbe28f800f1ee67351fa0235137f0c2b667fa8fd5e0c73bb4325a8cc8ad1e8e9bd597a'
